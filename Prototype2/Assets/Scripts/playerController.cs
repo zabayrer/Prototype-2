86bd79e090063d9ed42e0b87245f5f5be6d3d5cf
zabayrer <122053629+zabayrer@users.noreply.github.com>
2023-01-10 10:47:57 -0800

    Added prototype 2

---
+++ Prototype2/Assets/Scripts/playerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float boundary = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < -boundary)
+        {
+            transform.position = new Vector3(-boundary, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > boundary)
+        {
+            transform.position = new Vector3(boundary, transform.position.y, transform.position.z);
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+    }
+}
